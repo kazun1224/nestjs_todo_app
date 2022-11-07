ebeb3848749bd4681b7872592863002b57bebeda
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-07 23:22:49 +0900

    login&logoutの実装

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,11 +18,47 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() dto:AuthDto): Promise<Msg> {
-    return this.authService.signUp(dto)
+  signUp(@Body() dto: AuthDto): Promise<Msg> {
+    return this.authService.signUp(dto);
+  }
+
+  // nest.jsでは201がdefaultで返る
+  // 下記の記述で200が返るようにしてる
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(
+    @Body() dto: AuthDto,
+    /*
+    nest.jsはstandard modeで自動でjsonを返す仕様でcookieは使えない
+    @Resを使うととexpressの機能になり、cookieを使える
+    { passthrough: true }は両方の機能を使うときに設定
+    */
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Msg> {
+    const jwt = await this.authService.login(dto);
+    res.cookie('access_token', jwt.accessToken, {
+      httpOnly: true,
+      // secureはlocalではfalse 本番環境ではtrueでhttps通信にする必要がある
+      secure: false,
+      sameSite: 'none',
+      path: '/',
+    });
+    return {
+      message: 'ok',
+    };
   }
 
   @HttpCode(HttpStatus.OK)
-  @Post('login')
-  async login(@Body() dto:AuthDto @Res({passthrough:true}) res:Response): Promise<Msg>
+  @Post('logout')
+  logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Msg {
+    res.cookie('access_token', '', {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'none',
+      path: '/',
+    });
+    return {
+      message: 'ok',
+    };
+  }
 }
