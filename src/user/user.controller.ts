f0647d2a8e9db900bfd354b966394c31d5222e95
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-08 15:41:06 +0900

    ユーザーデータを取得endpointとユーザーデータを更新するendpointの作成

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Body, Get, Patch, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
+// UseGuards(AuthGuard())を使用するとjWTでプロテクトできる
+// 下記でuserの全てのエンドポイントでプロテクトできる
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
