7035cf39c12f2b706b757aa9e963c1d9c455d784
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2022-11-11 22:51:42 +0900

    adminjsのインストール

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,26 @@ import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from '@adminjs/nestjs';
+import * as AdminJSPrisma from '@adminjs/prisma';
+import AdminJS from 'adminjs';
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+};
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN);
+  }
+  return null;
+};
+
+AdminJS.registerAdapter({
+  Resource: AdminJSPrisma.Resource,
+  Database: AdminJSPrisma.Database,
+});
 
 @Module({
   imports: [
@@ -14,6 +34,24 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     TodoModule,
     PrismaModule,
+    AdminModule.createAdminAsync({
+      useFactory: () => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [],
+        },
+        auth: {
+          authenticate,
+          cookieName: 'adminjs',
+          cookiePassword: 'secret',
+        },
+        sessionOptions: {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret',
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
